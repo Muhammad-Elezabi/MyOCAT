0f763d80212a8ca07cd08feb8dfce4260f08425b
Muhammad Elezabi <ezabi8@gmail.com>
2021-07-28 15:34:53 -0400

    Task 1 Completed

--- API/src/microservices/Assessment-Service/index.js
+++ API/src/microservices/Assessment-Service/index.js
@@ -3,6 +3,27 @@ const { Assessments } = require(`../Database`);
 exports.submit = async (assessment) => {
   // use the bookshelf model Assessments from API/src/microservices/Database to save
   // the assessment data in the PostgreSQL database
+  assessment.created_at = new Date();
+  assessment.score = parseInt(assessment.judicialContact) + parseInt(assessment.catAltercations) +
+  parseInt(assessment.ownerAltercations) + parseInt(assessment.playsWell) + parseInt(assessment.Hisses);
+  assessment.cat_name = assessment.catName;
+  assessment.cat_date_of_birth = assessment.DOB;
+  if (assessment.score <= 1) {
+    assessment.risk_level = `Low`;
+  }
+  else if (assessment.score <= 3) {
+    assessment.risk_level = `Medium`;
+  }
+  else {
+    assessment.risk_level = `High`;
+  }
+  Assessments.forge({
+    score: assessment.score,
+    risk_level: assessment.risk_level,
+    cat_name: assessment.cat_name,
+    cat_date_of_birth: assessment.DOB,
+    created_at: assessment.created_at,
+  }).save();
 };
 
 exports.getList = () => {
